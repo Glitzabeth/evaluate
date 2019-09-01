35785e6fb372b7096a54863e1ff0dbb7002afecc
Omotayo Ikudayisi <omotayotitijames@gmail.com>
2019-09-01 21:15:12 +0100

    Do basic calculation
    inspired by cchub

---
+++ calc.js
@@ -0,0 +1,48 @@
+/**
+ * Author： https://omotayo.titi.netlify.com
+ * Date：1st of September, 2019
+ */
+
+//DOM expression display div in index.html
+let display = document.getElementById("display");
+display.innerHTML = 0;
+function pressBtn(btn) {
+  let pressed = btn.value;
+  let expr = display.innerHTML;
+  if (pressed === '=') {
+    //evaluate calculation
+    display.innerHTML = eval(expr);
+  }
+  else if (pressed==="<"){
+    display.innerHTML = display.innerHTML.substring(0, expr.length-1);
+  }
+  else if (pressed === ".") {
+    if (display.innerHTML.indexOf('.') !=-1 && display.innerHTML.match(/\/|\*|\+|-/g) === null || display.innerHTML.substring(expr.length-1, expr.length) == '.') {
+      display.innerHTML = display.innerHTML;
+    }
+    else {
+      display.innerHTML = display.innerHTML + pressed;
+    }
+  }
+   //evaluation expressiopn (RegExp)
+  else if (pressed.match(/\/|\*|\+|-/g)){
+    if (display.innerHTML.substring(expr.length-1, expr.length).match(/\/|\*|\+|-/g)){
+      display.innerHTML = display.innerHTML.replace(display.innerHTML.substring(expr.length-1, expr.length).match(/\/|\*|\+|-/g), pressed);
+    }
+    else {
+      display.innerHTML = display.innerHTML + pressed;
+    }
+  }
+  else if (pressed === 'C') {
+    display.innerHTML = '0';
+  }
+  else {
+    if (display.innerHTML === '0') {
+      display.innerHTML = pressed;
+    }
+    else {
+    display.innerHTML = display.innerHTML+btn.value;
+    }
+  }
+
+}
